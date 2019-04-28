24a582467aa2147dbf53d85ce842cf73c9dbc5d5
BeardedNoob <albert.jvrens@gmail.com>
2019-04-28 11:00:32 +0200

    further fixes and start of socket io

--- node-todo-api/server/routes/todo.routes.js
+++ node-todo-api/server/routes/todo.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const todoRouter = express.Router();
 const {ObjectID} = require('mongodb');
 const bcrypt = require('bcryptjs');
 const _ = require('lodash');
@@ -7,128 +6,131 @@ const _ = require('lodash');
 const {mongoose} = require('../db/mongoose');
 const {Todo} = require('../models/todo');
 
-todoRouter.post('/', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
+module.exports = function (io) {
+  const todoRouter = express.Router();
+
+  todoRouter.post('/', async (req, res) => {
+    try {
+      const todo = new Todo({
+        text: req.body.text
+      });
+
+      const savedTodo = await todo.save();
+
+      const newTodo = {
+        id: savedTodo._id,
+        text: savedTodo.text,
+        completed: false,
+        completedAt: savedTodo.completedAt
+      };
+
+      res.send(newTodo);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
   });
 
-  todo.save()
-    .then((doc) => {
-      const newTodo = {
-        id: doc._id,
-        text: doc.text,
-        completed: false,
-        completedAt: doc.completedAt
-      };
-
-      res.send(newTodo);
-    }, (err) => {
-      res.status(400).send(err);
-    });
-});
-
-todoRouter.get('/', (req, res) => {
-  Todo.find({})
-    .then((todos) => {
-      const temp = todos.map((todoItem) => {
-        return {
-          id: todoItem._id,
-          text: todoItem.text,
-          completed: todoItem.completed,
-          completedAt: todoItem.completedAt
-        };
+  todoRouter.get('/', async (req, res) => {
+    const todos = await Todo.find({})
+      .catch((error) => {
+        return res.status(400).send(error);
       });
-      res.send(temp);
-    }, (err) => {
-      res.status(400).send(err);
-    });
-});
 
-todoRouter.get('/:id', (req, res) => {
-  var todoId = req.params.id;
-
-  if (!ObjectID.isValid(todoId)) {
-    return res.status(404).send();
-  }
-
-  Todo.findOne({
-    _id: todoId
-  })
-    .then((todo) => {
-      if (!todo) {
-        return res.status(404).send();
-      }
-
-      const date = new Date(todo.completedAt);
-      const completedAtFormatted = todo.completedAt ? date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() : '';
-
-      const newTodo = {
-        id: todo._id,
-        text: todo.text,
-        completed: todo.completed,
-        completedAt: completedAtFormatted
+    const temp = todos.map((todoItem) => {
+      return {
+        id: todoItem._id,
+        text: todoItem.text,
+        completed: todoItem.completed,
+        completedAt: todoItem.completedAt
       };
-
-      res.send(newTodo);
-    })
-    .catch((err) => {
-      res.status(400).send();
     });
-});
+    res.send(temp);
+  });
 
-todoRouter.delete('/:id', async (req, res) => {
-  try {
+  todoRouter.get('/:id', async (req, res) => {
     const todoId = req.params.id;
 
     if (!ObjectID.isValid(todoId)) {
       return res.status(404).send();
     }
 
-    const todo = await Todo.findOneAndRemove({
+    const todo = await Todo.findOne({
       _id: todoId
-    });
+    })
+      .catch((error) => {
+        return res.status(400).send(error);
+      });
+
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    const date = new Date(todo.completedAt);
+    const completedAtFormatted = todo.completedAt ? date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() : '';
+
+    const newTodo = {
+      id: todo._id,
+      text: todo.text,
+      completed: todo.completed,
+      completedAt: completedAtFormatted
+    };
+
+    res.send(newTodo);
+  });
+
+  todoRouter.delete('/:id', async (req, res) => {
+    try {
+      const todoId = req.params.id;
+
+      if (!ObjectID.isValid(todoId)) {
+        return res.status(404).send();
+      }
+
+      const todo = await Todo.findOneAndRemove({
+        _id: todoId
+      });
+
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+
+  todoRouter.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    const todo = await Todo.findOneAndUpdate({
+        _id: id
+      },
+      {$set: body},
+      {new: true})
+      .catch((error) => {
+        return res.status(400).send(error);
+      });
 
     if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
+  });
 
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-todoRouter.patch('/:id', (req, res) => {
-  const id = req.params.id;
-  const body = _.pick(req.body, ['text', 'completed']);
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findOneAndUpdate({
-      _id: id
-    },
-    {$set: body},
-    {new: true})
-    .then((todo) => {
-      if (!todo) {
-        return res.status(404).send();
-      }
-
-      res.send({todo});
-    })
-    .catch((err) => {
-      res.status(400).send();
-    });
-});
-
-module.exports = todoRouter;
+  return todoRouter;
+};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -2,12 +2,17 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const todoRouter = require('./routes/todo.routes');
-const userRouter = require('./routes/user.routes');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const port = process.env.PORT;
+
+const todoRouter = require('./routes/todo.routes')(io);
+const userRouter = require('./routes/user.routes');
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
